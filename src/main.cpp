0c7755bb6a6bf37b6a709d3abe793906d1a1c397
Frederik Falk Nyboe <ffn@mmmi.sdu.dk>
2022-04-20 14:25:40 +0200

    go

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,19 @@ int main(int argc, char *argv[]) {
 
     std::vector<MagSample> samples = msf.GetSamples(1);
 
+    for (int i = 0; i < samples.size(); i++) {
+        for (int j = 0; j < 12; j++) {
+            std::cout << std::to_string(samples[i][j].data) << "\t";
+
+            if (j==2 || j==5 || j==8 ) {
+                std::cout << "\t";
+            }
+        }
+        std::cout << std::endl;
+
+    }
+
+
 
 
     return 0;
