e24ccbf1d02923c60d59a9acec3ed0872d474d4a
Frederik Falk Nyboe <ffn@mmmi.sdu.dk>
2022-04-20 14:31:49 +0200

    go

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Please pass the number of periods to sample as argument" << std::endl;
+        return 1;
+    }
+
+    unsigned int n_periods = std::stoi(argv[1]);
+
     MagSampleFetcher msf(1, 8192);
 
-    std::vector<MagSample> samples = msf.GetSamples(1);
+    std::vector<MagSample> samples = msf.GetSamples(n_periods);
 
     for (int i = 0; i < samples.size(); i++) {
         for (int j = 0; j < 12; j++) {
@@ -15,8 +22,8 @@ int main(int argc, char *argv[]) {
                 std::cout << "\t";
             }
         }
-        std::cout << std::endl;
 
+        std::cout << std::endl;
     }
 
 
