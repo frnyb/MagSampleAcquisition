f39e25cfddcc7a1427f113710f645acedd7fbffe
Frederik Falk Nyboe <ffn@mmmi.sdu.dk>
2022-05-10 15:21:21 +0200

    New version

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,27 @@
 //#include "../include/MagSampleFetcher_cpp/src/mag_sample_fetcher.h"
 #include "../include/SlidingWindowMagSampleFetcher_cpp/src/sliding_window_mag_sample_fetcher.h"
+#include "../include/BRAM-uio-driver/src/bram_uio.h"
 
 #include <iostream>
 
+#define 	N_PERIODS				10
+#define 	N_SAMPLES_PER_PERIOD	8
+#define 	N_CHANNELS				12
+#define		N_SAMPLES				N_PERIODS*N_SAMPLES_PER_PERIOD
+
 int main(int argc, char *argv[]) {
-    SlidingWindowMagSampleFetcher msf(1, 8192);
+    //SlidingWindowMagSampleFetcher msf(1, 8192);
+	BRAM bram(1, 8192);
 
-    std::vector<MagSample> samples;
+	bram[0] = 1;
 
-    if (argc == 2) {
-
-        unsigned int n_periods = std::stoi(argv[1]);
-
-        while(!msf.Start(n_periods));
-
-    } else {
-        
-        while(!msf.Start());
-
-    }
-
-    msf.GetSamples(&samples);
-
-    for (int i = 0; i < 12; i++) {
-        std::cout << "Time_ch" << std::to_string(i) << "\t";
-    }
-
-    for (int i = 0; i < 12; i++) {
-        std::cout << "Sample_ch" << std::to_string(i);
-
-        if (i != 11) {
-            std::cout << "\t";
-        }
-    }
+	while(bram[0] == 1) ;
 
     std::cout << std::endl;
 
-    for (int i = 0; i < samples.size(); i++) {
+    for (int i = 0; i < N_SAMPLES; i++) {
         for (int j = 0; j < 12; j++) {
-            std::cout << std::to_string(samples[i][j].time_offset) << "\t";
-        }
-
-        for (int j = 0; j < 12; j++) {
-            std::cout << std::to_string(samples[i][j].data);
-
-            if (j != 11) {
-                std::cout << "\t";
-            }
+            std::cout << std::to_string(bram[i*N_CHANNELS+j]) << "\t";
         }
 
         std::cout << std::endl;
